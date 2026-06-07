4fd89d8ef6151fdf0687e18d186b9f5c5504a634
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-07 23:29:55 +0530

    adding new file

---
+++ ultaAlphabetTriangle.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void print(int n){
+    for(int i=0; i<n; i++){
+        for(char ch='A'; ch<'A'+(n-i);ch++){
+            cout<<ch <<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+
+    int t;
+    cout<<"Enter number of testcases : ";
+    cin >>t;
+    for(int i=0; i<t; i++){
+        int n;
+        cout<<"Enter number of rows : ";
+        cin>>n;
+        print(n);
+    }
+    return 0;
+}
