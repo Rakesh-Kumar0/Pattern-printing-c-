2929e0d36ef1b3c0b1919b5771b18fc78b638235
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-07 23:45:51 +0530

    adding new file

---
+++ continuousAlphabetTriangle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void print(int n){
+    char ch = 'A';
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<ch <<" ";
+        }
+        cout<<endl;
+        ch += 1;
+    }
+}
+
+int main(){
+
+    int t;
+    cout<<"Enter number of testcases : ";
+    cin>>t;
+    for(int i=0; i<t; i++){
+        int n;
+        cout<<"Enter number of rows : ";
+        cin>>n;
+        print(n);
+    }
+    return 0;
+}
