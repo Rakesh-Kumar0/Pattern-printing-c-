5cde0560fcc3055a4531b9b5e7a52f9401909544
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-06-07 10:33:21 +0530

    adding new file

---
+++ joinedPyramid.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+void print1(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<(n-i-1); j++){
+            cout<<" ";
+        }
+        for(int k=0; k<(2*i+1); k++){
+            cout<<"*";
+        }
+        for(int l=0; l<(n-i-1); l++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void print2(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            cout<<" ";
+        }
+        for(int j=0; j<(2*n)-(2*i+1); j++){
+            cout<<"*";
+        }
+        for(int j=0; j<i; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+
+    int t;
+    cout<<"Enter number of testcase : ";
+    cin>>t;
+    for(int i=0; i<t; i++){
+        int n;
+        cout<<"Enter number of rows : ";
+        cin>>n;
+        print1(n);
+        print2(n);
+    }
+    return 0;
+}
